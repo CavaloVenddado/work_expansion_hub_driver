121131ec96196a06356e526848fd5c339f5b4bf9
Gabriel Pogorzelski <gabrielstpogo@gmail.com>
2026-05-15 18:52:44 -0300

    Valores de separacao das rodas alterados

--- src/config.hpp
+++ src/config.hpp
@@ -6,7 +6,10 @@
 #define TICK_PER_RADIANS TICK_PER_ROT/(2*3.1415)
 #define WHEEL_RADIUS 0.048
 
-#define WHEEL_SEPARATION_WIDTH 0.367
-#define WHEEL_SEPARATION_LENGTH 0.336
+//para todos que vierem depois de mim... os dois parametros abaixo (wheel separation width e length) nao sao a distancia de roda a roda, mas sim a metade desse
+//valor. agora porque ta com um nome enganoso, nao sei... mas nao se voce estiver lendo isso, não cometa o mesmo erro que eu (ficar 1 mês tentando descobrir pq
+// o robo nao gira direito, pra ver que esses valores sao, na verdade, da roda até o centro do robô)
+#define WHEEL_SEPARATION_WIDTH 0.1835 //0.367
+#define WHEEL_SEPARATION_LENGTH 0.168 //0.336
 
 #define L_SUM 10
